843cec7939fdf27c510f7f096235c3a79c431832
momin56 <mominyt56@gmail.com>
2025-07-23 02:03:09 +0500

    deploying frontend

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://e-commerce-backend-production-abe1.up.railway.app/",
+  baseURL: "https://e-commerce-backend-production-abe1.up.railway.app",
   withCredentials: true,
 });
 
