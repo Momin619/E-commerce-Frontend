d756a382d8b0c526f5ee80c21fb7abd180f66f48
momin56 <mominyt56@gmail.com>
2025-07-23 01:55:57 +0500

    deploying frontend

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: "https://e-commerce-backend-production-abe1.up.railway.app/",
   withCredentials: true,
 });
 
