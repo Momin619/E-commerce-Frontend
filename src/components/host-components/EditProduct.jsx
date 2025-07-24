f16fdd2a58df2ff2d90b6c3995f7e2ab4fec8406
momin56 <mominyt56@gmail.com>
2025-07-24 22:57:29 +0500

    Cart Frontend

--- src/components/host-components/EditProduct.jsx
+++ src/components/host-components/EditProduct.jsx
@@ -219,7 +219,7 @@ function EditProduct() {
         {/* Submit Button */}
         <button
           type="submit"
-          className="w-full bg-blue-600 text-white font-medium py-2.5 rounded-md hover:bg-blue-700 transition duration-300 text-sm sm:text-base"
+          className="w-full cursor-pointer bg-blue-600 text-white font-medium py-2.5 rounded-md hover:bg-blue-700 transition duration-300 text-sm sm:text-base"
         >
           Edit Product
         </button>
